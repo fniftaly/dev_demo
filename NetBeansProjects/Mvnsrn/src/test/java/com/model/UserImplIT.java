74de1ece37df63c929dba711b7c5acf6ac2a2fd7
fniftaly <atbulag@yahoo.com>
2016-10-16 13:24:21 -0700

    Update UserImplIT.java

--- NetBeansProjects/Mvnsrn/src/test/java/com/model/UserImplIT.java
+++ NetBeansProjects/Mvnsrn/src/test/java/com/model/UserImplIT.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.model;
 
 import java.util.HashMap;
@@ -16,6 +12,7 @@ import static org.junit.Assert.*;
 /**
  *
  * @author def
+   added remote file
  */
 public class UserImplIT {
     
